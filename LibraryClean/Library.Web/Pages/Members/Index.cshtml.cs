40b65d5345b00d9280adcb89e49745281436de5d
Kuba <jakub.furmanski@microsoft.wsei.edu.pl>
2025-09-03 12:47:30 +0200

    some fix

--- LibraryClean/Library.Web/Pages/Members/Index.cshtml.cs
+++ LibraryClean/Library.Web/Pages/Members/Index.cshtml.cs
@@ -1,6 +1,8 @@
 using Library.Web.Models;
 using Library.Web.Utils;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
 
 namespace Library.Web.Pages.Members;
 
@@ -10,11 +12,27 @@ public class IndexModel : PageModel
     private readonly IHttpClientFactory _http;
     public IndexModel(IHttpClientFactory http) => _http = http;
 
-    public async Task OnGet()
+    public async Task<IActionResult> OnGet()
     {
         var client = ApiClientFactory.Create(HttpContext, _http);
         var resp = await client.GetAsync("/api/Members");
-        resp.EnsureSuccessStatusCode();
+
+        // Not logged in -> go to Login
+        if (resp.StatusCode == HttpStatusCode.Unauthorized)
+            return Challenge();         // cookie auth will redirect to /Account/Login with returnUrl
+
+        // Logged in but not allowed -> go to AccessDenied
+        if (resp.StatusCode == HttpStatusCode.Forbidden)
+            return Forbid();            // cookie auth will redirect to AccessDeniedPath
+
+        // Any other non-success: show empty list instead of throwing
+        if (!resp.IsSuccessStatusCode)
+        {
+            Members = new();
+            return Page();
+        }
+
         Members = await resp.Content.ReadFromJsonAsync<List<MemberDto>>() ?? new();
+        return Page();
     }
 }
