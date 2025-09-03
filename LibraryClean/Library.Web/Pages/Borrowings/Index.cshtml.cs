3792ccc782fcdeb8a6f6db9786cba4fd27a77b96
Kuba <jakub.furmanski@microsoft.wsei.edu.pl>
2025-09-03 13:12:09 +0200

    fix

--- LibraryClean/Library.Web/Pages/Borrowings/Index.cshtml.cs
+++ LibraryClean/Library.Web/Pages/Borrowings/Index.cshtml.cs
@@ -1,6 +1,8 @@
 using Library.Web.Models;
 using Library.Web.Utils;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
 
 namespace Library.Web.Pages.Borrowings;
 
@@ -10,11 +12,24 @@ public class IndexModel : PageModel
     private readonly IHttpClientFactory _http;
     public IndexModel(IHttpClientFactory http) => _http = http;
 
-    public async Task OnGet()
+    public async Task<IActionResult> OnGet()
     {
         var client = ApiClientFactory.Create(HttpContext, _http);
         var resp = await client.GetAsync("/api/Borrowings");
-        resp.EnsureSuccessStatusCode();
+
+        if (resp.StatusCode == HttpStatusCode.Unauthorized)
+            return Challenge();   // -> sends to /Account/Login with returnUrl
+
+        if (resp.StatusCode == HttpStatusCode.Forbidden)
+            return Forbid();      // -> sends to AccessDeniedPath
+
+        if (!resp.IsSuccessStatusCode)
+        {
+            Items = new();
+            return Page();        // or show an error message if you prefer
+        }
+
         Items = await resp.Content.ReadFromJsonAsync<List<BorrowingDto>>() ?? new();
+        return Page();
     }
 }
